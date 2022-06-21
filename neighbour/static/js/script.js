04cb95b8af31dd341f83fccc8310d7383210a309
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-06-21 13:23:57 +0300

    Added js for autotyping

---
+++ neighbour/static/js/script.js
@@ -0,0 +1,12 @@
+let typed = new Typed(".auto-type", {
+  strings: [
+    "Says Hi to everyone",
+    "Brings Happiness for all ",
+    "makes it happen!",
+    "Cares for your Security",
+    "Makes it feel like Home",
+  ],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
